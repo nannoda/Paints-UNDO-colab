9c6b200ee22ca44971d4309d3f10ddf7878d6a28
Nan Noda <114621472+nannoda@users.noreply.github.com>
2025-04-09 13:45:56 -0500

    Make it a package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paints_undo",
+    version="0.1.0",
+    packages=["diffusers_helper", "diffusers_vdm"],
+    include_package_data=True,
+)
